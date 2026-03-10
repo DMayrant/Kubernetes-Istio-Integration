99c56d8cbf6749884d42c47fa497d81320053ea9
Devonne Mayrant <DMayrant@icloud.com>
2026-03-10 16:48:52 +0700

    Update istio.sh

--- Logs/istio.sh
+++ Logs/istio.sh
@@ -2,7 +2,7 @@
 set -euo pipefail 
 
 # Installing Istio Service Mesh 🛜
-echo "Installing Istion"
+echo "Installing Istio"
 git clone https://github.com/istio/istio.git
 
 helm repo add istio https://istio-release.storage.googleapis.com/charts
@@ -19,11 +19,15 @@ helm install istiod istio/istiod -n istio-system
 kubectl label namespace default istio-injection=enable --overwrite
 kubectl get ns -L istio-injection 
 kubectl get ns default --show-labels 
+echo "Install complete ✅"
 
 # Uninstall istio service-mesh and remove ns label
+echo "Uninstalling Istio"
 helm uninstall istio-base istio/base -n istio-system  
 kubectl label namespace default istio-injection=enable-
-echo "Install complete ✅"
+kubectl get ns default --show-lables
+echo "Uninstall complete ✅"
+
 
 
 
