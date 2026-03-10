c85fa1b5aa5e9bbb0e9449948dab39cf2be2422a
DMayrant <DMayrant@icloud.com>
2026-03-10 16:38:47 +0700

    istio service mesh

--- Logs/istio.sh
+++ Logs/istio.sh
@@ -1,43 +1,27 @@
-# Istio Service mesh 
-This service provides your workloads with additional 
+#!/bin/bash 
+set -euo pipefail 
 
-- Observability 🔭
-- mTLS 🔐
-- Traffic Control 🎛️
-- Policy Enforcement and Security  
-  
 # Installing Istio Service Mesh 🛜
-
 git clone https://github.com/istio/istio.git
 
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 
-
 # Updating repo 
 helm repo update 
 
-
 # Creating namespace and installing istio base chart and pod 📈
-
 kubectl create namespace istio-system
-
 helm install istio-base istio/base -n istio-system 
-
 helm install istiod istio/istiod -n istio-system
 
 # Label namespace 
-
-
 kubectl label namespace default istio-injection=enable --overwrite
-
-
-# Uninstall istio service-mesh and remove ns label 
-
-kubectl label namespace default istio-injection=enable-
-
 kubectl get ns -L istio-injection 
+kubectl get ns default --show-labels 
 
-helm uninstall istio-base istio/base -n istio-system 
+# Uninstall istio service-mesh and remove ns label
+helm uninstall istio-base istio/base -n istio-system  
+kubectl label namespace default istio-injection=enable-
 
 
 
